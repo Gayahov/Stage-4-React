97827c47df9e93ad26d5e8feed592164728be22f
Gayahov <gayahov@gmail.com>
2019-03-18 20:09:26 +0400

    test

---
+++ popup_sass/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.scss';
+///import Popup from './component/popup';
+import Project from './component';
+
+class App extends Component {
+  render() {
+    return (
+      <Project/>
+      ///<Popup/>
+
+    );
+  }
+}
+
+export default App;
---
+++ popup_sass/src/component/index.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+//nimport './style_header.scss';
+
+
+class Project extends Component {
+    render(){
+        return(
+            <div className= "container">
+
+            </div>
+
+        
+        )
+    }
+}
+export default Project;
+  
+  
+  
+  
